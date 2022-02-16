ea1bdd9cbcdec79a817532b3d2fb89c7f3d4674e
Michael Tran <michaelhtran120@gmail.com>
2022-02-16 08:29:35 -0800

    reorg page files and added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
+// React / Library imports
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home.tsx";
+import Dashboard from "./pages/Dashboard/Dashboard.tsx";
+import LogIn from "./pages/LogIn/LogIn.tsx";
+import SignUp from "./pages/SignUp/SignUp.tsx";
+import NavHeader from "./components/NavHeader/NavHeader.tsx";
+
+// Style imports
+
+// Component imports
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <NavHeader />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="login" element={<LogIn />} />
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/NavHeader/NavHeader.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import styles from "./NavHeader.module.css";
+
+function NavHeader() {
+  const [user, setUser] = useState(false);
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.left_links}>
+        <div className={styles.logo}>
+          <h1>MERN-Task</h1>
+        </div>
+        <NavLink to="/">Home</NavLink>
+      </nav>
+      <nav className={styles.right_links}>
+        <NavLink to="login">Log In</NavLink>
+        <NavLink to="signup">Sign Up</NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default NavHeader;
