2d97a222618fed15ceeafa22a03a52843481ba44
Michael Tran <michaelhtran120@gmail.com>
2022-02-24 15:21:11 -0800

    update modal styling

--- client/src/components/EditTaskModal/EditTaskModal.jsx
+++ client/src/components/EditTaskModal/EditTaskModal.jsx
@@ -5,9 +5,12 @@ import Modal from "react-modal";
 // Component imports
 import EditTaskForm from "../Forms/EditTaskForm/EditTaskForm";
 
+// Style imports
+import styles from "./EditTaskModal.module.css";
+
 function EditTaskModal({ isModalOpen, taskData, handleCloseModal }) {
   return (
-    <Modal isOpen={isModalOpen} >
+    <Modal isOpen={isModalOpen} className={styles.edit_task_modal} overlayClassName={styles.modal_overlay}>
       <EditTaskForm taskData={taskData} handleCloseModal={handleCloseModal} />
     </Modal>
   );
