ee9c6ce5173b3219909a906e6e0866085ec2dcc3
Michael Tran <michaelhtran120@gmail.com>
2022-02-26 08:04:06 -0800

    fix sendFile typo

--- server/server.js
+++ server/server.js
@@ -29,8 +29,9 @@ app.use("/api/users", userRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../", "client", "index", "html"));
+    res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
   });
 }
 
